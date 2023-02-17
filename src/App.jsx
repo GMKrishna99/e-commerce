d227f20a965105399fa3de82b32c82246b55fad3
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-17 11:52:52 +0530

    chore : replaced itemlist to home component

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { BrowserRouter as Router,
   Switch,
   Link,
 } from 'react-router-dom';
+import Home from './components/Pages/Home/Home';
 
 function App() {
   
@@ -24,7 +25,7 @@ function App() {
         <NavBar/>
           <Switch>
             <Route exact path="/">
-              <ItemList/>
+              <Home/>
             </Route>
             <Route path="/about">
               <About/>
