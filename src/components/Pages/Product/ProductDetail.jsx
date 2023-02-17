436b502a6b6eb1ddec6bc221c2af7bbeaae6eb3f
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-17 18:20:32 +0530

    feat : added new product page

---
+++ src/components/Pages/Product/ProductDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './ProductDetail.css'
+import { useParams } from 'react-router-dom'
+
+function ProductDetail() {
+    let { id } = useParams();
+  return (
+    <div>
+        <h1>Product Detail</h1>
+        <p>Product ID : {id}</p>
+
+         
+    </div>
+  )
+}
+
+export default ProductDetail
