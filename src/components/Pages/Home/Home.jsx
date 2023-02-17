b1721878f4cdd50dbcecd157dec0152cb4cc4fa8
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-17 12:36:05 +0530

    feat : recommendation sectin added

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './Home.css'
 import ItemList from '../../organisms/ItemList/ItemList'
 import HomeBanner from '../../molecules/HomeBanner/HomeBanner'
+import RecommendationList from '../../organisms/Recommendation/RecommendationList'
 
 function Home() {
   return (
     <div>
         <HomeBanner/>
+        <RecommendationList/>
         <ItemList/>
     </div>
   )
---
+++ src/components/organisms/Recommendation/RecommendationList.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './RecommendationList.css'
+import axios from 'axios';
+import { Container } from 'react-bootstrap';
+import ItemCard from '../../molecules/itemcard/ItemCard';
+
+function RecommendationList() {
+    const [itemsList, setItemsList] = React.useState([]);
+   React.useEffect( () => {
+     axios.get("https://react-app-37960-default-rtdb.asia-southeast1.firebasedatabase.app/c2c.json")
+      .then(response => {
+        let itemsList = [];
+        for (let key in response.data){
+          itemsList.push(response.data[key])
+        }
+        setItemsList(itemsList)
+      })  
+  }, [])
+  return (
+    <div>
+        <Container>
+            <section className="recommendation-section">
+
+                    <h4>Recommendations</h4>
+            <div className="recommendation-list">
+                <div className="recommendation-list-slider">
+                {
+              itemsList.length === 0 
+              ? <h2>No items Found</h2>:
+              itemsList.map((item , index) => (
+                <ItemCard
+                  itemImg={item.itemImg}
+                  itemPrice={item.itemPrice}
+                  itemTitle={item.itemTitle}
+                  itemDes={item.itemDes}
+                  itemPlace={item.itemPlace}
+                  itemDate={item.itemDate}
+                />
+              ))
+            }
+                </div>
+            
+            </div>
+            </section>
+        </Container>
+    </div>
+  )
+}
+
+export default RecommendationList
