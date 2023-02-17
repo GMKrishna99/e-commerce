fdcd7f274b3dad008eaff3b0eae290ecb29064b8
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-17 10:00:37 +0530

    chore : Home component created

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+        {/*
+             header
+             scroller
+             itemlist 
+         */}
+    </div>
+  )
+}
+
+export default Home
