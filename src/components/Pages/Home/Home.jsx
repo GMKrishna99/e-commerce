95f330f08426739e45ebd3327bc335af06cb51d0
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-17 11:55:07 +0530

    feat : home banner added

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Home.css'
+import ItemList from '../../organisms/ItemList/ItemList'
+import HomeBanner from '../../molecules/HomeBanner/HomeBanner'
 
 function Home() {
   return (
     <div>
-        {/*
-             header
-             scroller
-             itemlist 
-         */}
+        <HomeBanner/>
+        <ItemList/>
     </div>
   )
 }
