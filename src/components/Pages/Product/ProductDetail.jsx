7010f7973a0fbab2c0b7611633c9e1f964785610
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-18 11:08:41 +0530

    chore: some css changes

--- src/components/Pages/Product/ProductDetail.jsx
+++ src/components/Pages/Product/ProductDetail.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 import { Col, Container, Row } from 'react-bootstrap';
 import axios from 'axios';
 
-function ProductDetail() {
+function ProductDetail({addToCart}) {
     let { id } = useParams();
     const [productDetails, setProductDetails] = React.useState('');
     useEffect(() => {
@@ -28,11 +28,10 @@ function ProductDetail() {
               <Col className='product-detail-col-r'>
                 <h1>{productDetails.itemTitle}</h1>
                 <p className='product-detail_desc'>{productDetails.itemDes}</p>
-
                 <div className='product-detail_price'>
                   <div className="product-detail_price_main">
                     <p className='product-detail_price_current'>
-                      ₹ {productDetails.itemPrice * 0.85}
+                      ₹ {productDetails.itemPrice * 0.8}
                     </p>
                     <p className='product-detail_price_original'>
                       ₹ {productDetails.itemPrice }
@@ -69,7 +68,9 @@ function ProductDetail() {
                 </div>
 
                 <div className='product-detail_btns'>
-                  <button className='product-detail_btn product-detail_btn_buy'>
+                  <button className='product-detail_btn product-detail_btn_buy'
+                    onClick={() => addToCart(productDetails)}
+                  >
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                   </svg>
