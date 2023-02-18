d964a7a24aa93ca05b541fdb98b5aac0eab3dc23
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-18 11:38:30 +0530

    chore: cart pill & cass added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './components/Pages/Contact/Contact';
 import SignUp from './components/Pages/Signup/SignUp';
 import SignIn from './components/Pages/SignIn/SignIn';
 import ProductDetail from './components/Pages/Product/ProductDetail';
-
+import React from 'react';
 import { BrowserRouter as Router, 
   Route , 
   Switch,
@@ -15,15 +15,18 @@ import { BrowserRouter as Router,
 import Home from './components/Pages/Home/Home';
 import Cart from './components/Pages/Cart/Cart';
 function App() {
-  const [cartList, setCartList] = React.useState([]);
+  const [cartList, setCartList] = React.useState([])
 
   const addToCart = (item) => {
-    setCartList([...cartList, item]);
+    setCartList([...cartList, item])
   }
+
   return (
     <div className="App">
       <Router>
-        <NavBar/>
+        <NavBar
+          cartList={cartList}
+        />
           <Switch>
             <Route exact path="/">
               <Home/>
@@ -41,8 +44,10 @@ function App() {
               <SignIn/>
             </Route>
             <Route path="/product/:id">
-            <ProductDetail/>
-             addTocart={addToCart}
+            <ProductDetail
+              addToCart={addToCart}
+            />
+            
           </Route>
           <Route path="/cart">
             <Cart/>
