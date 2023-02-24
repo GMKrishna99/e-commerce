330bbaebfa0d40e5afbe629f1fda519e9aa950b8
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-24 12:25:41 +0530

    chore: added some Components in Home Page

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -3,6 +3,12 @@ import './Home.css'
 import ItemList from '../../organisms/ItemList/ItemList'
 import HomeBanner from '../../molecules/HomeBanner/HomeBanner'
 import RecommendationList from '../../organisms/Recommendation/RecommendationList'
+import ItemListBooks from '../../organisms/ItemListBooks/ItemListBooks'
+import ItemListGames from '../../organisms/ItemListGames/ItemListGames'
+import ItemListMobiles from '../../organisms/ItemListMobiles/ItemListMobiles'
+import ItemListGym from '../../organisms/ItemListGym/ItemListGym'
+import ItemListTv from '../../organisms/ItemListTv/ItemListTv'
+import ItemListLaptop from '../../organisms/ItemListLaptop/ItemListLaptop'
 
 function Home() {
   return (
@@ -10,6 +16,12 @@ function Home() {
         <HomeBanner/>
         <RecommendationList/>
         <ItemList/>
+        <ItemListBooks/>
+        <ItemListTv/>
+        <ItemListMobiles/>
+        <ItemListLaptop/>
+        <ItemListGym/>
+        <ItemListGames/>
     </div>
   )
 }
