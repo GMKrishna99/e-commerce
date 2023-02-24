f26444af46e4e0a692afc5e5801a0db25647837a
Mohan Krishna <113287292+GMKrishna99@users.noreply.github.com>
2023-02-24 12:24:50 +0530

    chore: added new APi for recommend

--- src/components/organisms/Recommendation/RecommendationList.jsx
+++ src/components/organisms/Recommendation/RecommendationList.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 function RecommendationList() {
     const [itemsList, setItemsList] = React.useState([]);
    React.useEffect( () => {
-     axios.get("https://react-app-37960-default-rtdb.asia-southeast1.firebasedatabase.app/c2c.json")
+     axios.get("https://click-to-cart-e-commerce-default-rtdb.asia-southeast1.firebasedatabase.app/c2c.json")
       .then(response => {
         let itemsList = [];
         for (let key in response.data){
@@ -35,8 +35,6 @@ function RecommendationList() {
                   itemPrice={item.itemPrice}
                   itemTitle={item.itemTitle}
                   itemDes={item.itemDes}
-                  itemPlace={item.itemPlace}
-                  itemDate={item.itemDate}
                 />
                 </Link>
               ))
